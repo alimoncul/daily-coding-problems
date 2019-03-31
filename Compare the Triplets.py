905c4d1c9bcac7c288b69a9cc223ffd433873712
Alim Öncül <aoncul@stu.comu.edu.tr>
2019-03-31 14:40:07 +0300

    Compare the Triplets solution added.

---
+++ Compare the Triplets.py
@@ -0,0 +1,37 @@
+'''
+Compare the Triplets: https://www.hackerrank.com/challenges/compare-the-triplets/problem
+'''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    points = [0,0]
+    for i in range(len(a)):
+        if(a[i]>b[i]):
+            points[0] += 1
+        elif(a[i]<b[i]):
+            points[1] += 1
+        else:
+            continue
+    return points
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
